919e521c51c023def0b810b7cce40287fcc2f85b
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-27 02:41:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,26 +4,26 @@ class Dog
 
   @@all = []
 
-  def initialize(name)
-    @name=name
-    self.save
-  end
+    def initialize(name)
+      @name=name
+      self.save
+    end
 
-  def save
-    @@all << self
-  end
+    def save
+      @@all << self
+    end
 
-  def self.all
-    @@all
-  end
+    def self.all
+      @@all
+    end
 
-def self.clear_all
-  @@all = []
-end
+    def self.clear_all
+      @@all = []
+    end
 
-def self.print_all
-  @@all.each{|dog| puts dog.name}
-end
+    def self.print_all
+      @@all.each{|dog| puts dog.name}
+    end
 
 
 end
